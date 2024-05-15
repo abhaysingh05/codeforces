dc485ff640a8494d5fdbde1bf3b7be489b8996c8
Abhay Singh <abhaysingh0948@gmail.com>
2024-05-16 01:52:58 +0530

    New_Unsolved_question

--- temp.cpp
+++ temp.cpp
@@ -1,21 +1,20 @@
 #include <bits/stdc++.h>
+#define long long long int
 using namespace std;
-#define ff first
-#define ss second
-#define pb push_back
-#define all(x) x.begin(), x.end()
-#define int long long
-const int INF = (int)1e9;
-const int mod = INF + 7;
-/**************************************************************************************/
+constexpr long inf = (long)1e+18;
+constexpr int mod = 1000000007;
+
+// @author: ZhockDen
 
 void runCase(int &testCase) {
     // cout << "#Case " << testCase << ": \n";
 
-    // Your code here
+    
 }
 
-int32_t main() {
+int main() {
+
+    ios::sync_with_stdio(false);
 
     int tests = 1;
     cin >> tests;
--- temp1.cpp
+++ temp1.cpp
@@ -1,46 +1,61 @@
-#include <bits/stdc++.h>
-using namespace std;
-#define ff first
-#define ss second
-#define pb push_back
-#define all(x) x.begin(), x.end()
-#define int long long
-const int INF = (int)1e9;
-const int mod = INF + 7;
-/**************************************************************************************/
-int lis(vector<int>& a) {
-    int n = a.size();
-    vector<int> d(n, 1);
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < i; j++) {
-            if (a[j] < a[i])
-                d[i] = max(d[i], d[j] + 1);
+class DisjointSet {
+public:
+    vector<int> rank, parent;
+    DisjointSet(int n) {
+        rank.resize(n + 1, 0);
+        parent.resize(n + 1);
+        for (int i = 0; i <= n; i++) parent[i] = i;
+    }
+    int findUpar(int node) {
+        if (node == parent[node]) return node;
+        return parent[node] = findUpar(parent[node]);
+    }
+    void Unionbyrank(int u, int v) {
+        int u_v = findUpar(u);
+        int u_u = findUpar(v);
+        if (u_v == u_u) return;
+        if (rank[u_v] > rank[u_u])
+            parent[u_u] = u_v;
+        else if (rank[u_v] < rank[u_u])
+            parent[u_v] = u_u;
+        else {
+            parent[u_v] = u_u;
+            rank[u_u]++;
         }
     }
-
-    int ans = d[0];
-    for (int i = 1; i < n; i++) {
-        ans = max(ans, d[i]);
+};
+class Solution {
+public:
+    vector<vector<string>> accountsMerge(vector<vector<string>> &accounts) {
+        int n = accounts.size();
+        unordered_map<string, int> helper;
+        DisjointSet ds(n);
+        for (int i = 0; i < n; i++) {
+            for (int j = 1; j < accounts[i].size(); j++) {
+                string mail = accounts[i][j];
+                if (helper.find(mail) == helper.end())
+                    helper[mail] = i;
+                else
+                    ds.Unionbyrank(i, helper[mail]);
+            }
+        }
+        vector<vector<string>> ans;
+        vector<string> merged[n];
+        for (auto it : helper) {
+            string mail = it.first;
+            int node = ds.findUpar(it.second);
+            merged[node].push_back(mail);
+        }
+        for (int i = 0; i < n; i++) {
+            if (merged[i].size() == 0) continue;
+            sort(merged[i].begin(), merged[i].end());
+            vector<string> temp;
+            temp.push_back(accounts[i][0]);
+            for (auto it : merged[i]) {
+                temp.push_back(it);
+            }
+            ans.push_back(temp);
+        }
+        return ans;
     }
-    return ans;
-}
-void runCase(int &testCase) {
-    // cout << "#Case " << testCase << ": \n";
-
-    int n;
-    cin >> n;
-    vector<int> arr(n);
-    for(auto &e: arr) cin >> e;
-    int k;
-    cin >> k;
-    cout << lis(arr) << "\n";
-}
-
-int32_t main() {
-
-    int tests = 1;
-    // cin >> tests;
-    for (int i = 1; i <= tests; i++) runCase(i);
-
-    return 0;
-}
+};
